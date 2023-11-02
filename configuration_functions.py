d79357aefe939f0922f19ea5546096e149503eb5
Karl Mogensen <km@bleau.dk>
2023-11-02 11:01:40 +0100

    first commit

---
+++ configuration_functions.py
@@ -0,0 +1,15 @@
+
+
+def validate_file_content(fileContent):
+    #Perform validation on fileContent here, and return validation result 
+    return "File format was succesfully validated";
+
+def convert_file_content_to_list_of_lists(fileContent):
+    #Perform file conversion to object
+    return [
+        [1, 2],
+        [2, 8],
+        [8, 1],
+        [1, 12]
+    ]
+
---
+++ simulation_functions.py
@@ -0,0 +1,6 @@
+
+update_procedures = ["Ordered","MaxViolation","MonteCarlo"]
+
+def get_update_procedures():
+    return update_procedures
+
